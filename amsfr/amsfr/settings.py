b4f4f2feb26953718734832866445626c6a2c9ea
Val Melbin Cabitac <valcabitac011@gmail.com>
2022-12-31 02:21:43 +0800

    fix database in settings.py

--- amsfr/amsfr/settings.py
+++ amsfr/amsfr/settings.py
@@ -80,7 +80,6 @@ WSGI_APPLICATION = 'amsfr.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql',
-        'NAME': BASE_DIR / 'db.sqlite3',
         'NAME': 'amsfr_db',
         'USER': 'root',
         'PASSWORD': '',
